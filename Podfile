e1a514575f81ead6a3479e0a3a1ea1b57bea2faf
aenand15 <amitenand@gmail.com>
2019-02-16 18:51:09 -0500

    pod file and sonification code

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'sonification' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for sonification
+	pod 'AudioKit', '~> 4.6'
+  target 'sonificationTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'sonificationUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- sonification/GameViewController.swift
+++ sonification/GameViewController.swift
@@ -9,27 +9,46 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import AudioKit
 
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
-                
+
                 // Present the scene
                 view.presentScene(scene)
+                
             }
-            
+           
             view.ignoresSiblingOrder = true
             
             view.showsFPS = true
             view.showsNodeCount = true
         }
+    }
+    func startSound(){
+        let oscillator = AKOscillator()
+        AudioKit.output = oscillator
+        do{
+            try AudioKit.start()
+        } catch{
+            print("fail")
+        }
+        oscillator.start()
+        sleep(3)
+        oscillator.stop()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        startSound()
     }
 
     override var shouldAutorotate: Bool {
