26fb12874e285d00647c3228386e9e93fd30d8a6
aenand15 <amitenand@gmail.com>
2019-03-27 12:17:50 -0400

    testflight

--- sonification/GameScene.swift
+++ sonification/GameScene.swift
@@ -18,33 +18,23 @@ class GameScene: SKScene {
     private var engine = AudioEngine()
     private var shape = SKShapeNode()
     private var pathToDraw = UIBezierPath(roundedRect: CGRect(x: 100, y: 50, width: 1000, height: 50), cornerRadius: 16).cgPath
-    //private var pathToDraw : CGPath?
     
     override func didMove(to view: SKView) {
-//        let line1 = Line(start: CGPoint(x:(100.0), y:(100.0)), end: CGPoint(x:(view.frame.maxX-100.0), y:(view.frame.maxY-100.0)),color: SKColor.red )
-//        lines.append(line1)
-//        pathToDraw.move(to: line1.start)
-//        pathToDraw.addLine(to: line1.end)
-//        pathToDraw.closeSubpath()
-//        yourline.path = pathToDraw
-//        yourline.strokeColor = line1.color
-//        yourline.lineWidth = 50.0
-        //yourline.fillColor = UIColor.blue
 
-        //yourline.isUserInteractionEnabled = true
-        //addChild(yourline)
-//        var aRect = CGRect(x: 100, y: 100, width: 1000, height: 50);
-//        aRect = getBoundingRectAfterRotation(rect: aRect, angle: 45);
-//        pathToDraw = UIBezierPath(roundedRect: aRect, cornerRadius: 16).cgPath
         shape.path = pathToDraw
         shape.fillColor = UIColor.red
         shape.strokeColor = UIColor.red
         shape.lineWidth = 1
-        shape.zRotation = 0.436332
+        shape.position = CGPoint(x: 200, y: 10)
+        shape.zRotation = CGFloat(deg2rad(90))
         addChild(shape)
 
     }
-
+    
+    func deg2rad(_ number: Double) -> Double {
+        return number * .pi / 180
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let position = touch.location(in: self)
